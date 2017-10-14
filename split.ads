6b730b33b47221c73d9b541dbcdc176ad861d9fb
Julio <JulioToboso@gmail.com>
2017-10-14 18:34:01 +0200

    Update split.ads

--- split.ads
+++ split.ads
@@ -10,10 +10,11 @@ package Split is
 	package T_IO renames Ada.Text_IO;
 	package UString renames Ada.Strings.Unbounded;	
 	
+function Clean_Word (Word_In:String) return UString.Unbounded_String ;
+	
 procedure Split_Line_In_Words(Frase: in out UString.Unbounded_String;
 				List: in out word_lists.Word_List_Type);
 
-	
 procedure Split_Text_In_Words(Fichero: in T_IO.File_Type;
 				List: in out word_lists.Word_List_Type);
 	
