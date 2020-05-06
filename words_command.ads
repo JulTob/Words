6a9576c75a084453ed15c6435a6be42894c7ff0d
Julio <JulioToboso@gmail.com>
2020-05-06 18:16:13 +0200

    Update words_command.ads

--- words_command.ads
+++ words_command.ads
@@ -6,14 +6,17 @@ with Word_Lists;
 
 package Words_Command is
 
-    	package T_IO renames Ada.Text_IO;
-    	package ACL renames Ada.Command_Line;
-        package I_IO is new Ada.Text_IO.Integer_IO(Integer);
+	package T_IO 	renames Ada.Text_IO;
+	package ACL 	renames Ada.Command_Line;
+	package I_IO 	is new  Ada.Text_IO.Integer_IO(Integer);
 
-procedure Dispose_List (Word_List: in out Word_Lists.Word_List_Type);
+	procedure 
+		Dispose_List 	(Word_List: in out Word_Lists.Word_List_Type);
 
-procedure Command_Input (selector: in out integer);
+	procedure 
+		Command_Input	(selector: in out integer);
 
-procedure Interactive_Mode (List: in out Word_Lists.Word_List_Type);
+	procedure 
+		Interactive_Mode	(List: 	in out Word_Lists.Word_List_Type);
 
-end Words_Command;
+	end Words_Command;
