b0da638644e79f7118bae342db327fb55941410f
Julio <JulioToboso@gmail.com>
2020-05-07 11:11:47 +0200

    Update linea_comandos.adb

--- linea_comandos.adb
+++ linea_comandos.adb
@@ -4,26 +4,22 @@ with Ada.Command_Line;
 
 procedure Linea_Comandos is
 
-   package ACL renames Ada.Command_Line;
-	package ASU renames Ada.Strings.Unbounded;
+	package ACL	renames Ada.Command_Line;
+	package ASU	renames Ada.Strings.Unbounded;
 	use type ASU.Unbounded_String;
 
 	Mi_Excepcion: exception;
 	
-begin
-	
-	
-	
-	if ACL.Argument_Count /= 2 then
-		raise Mi_Excepcion;
-	end if;
-	
-	Ada.Text_Io.Put_Line(ACL.Argument(2));
-	
-exception
-	when Mi_Excepcion => 
-		Ada.Text_Io.Put_Line("Pon al menos un argumento");
-	
-end Linea_Comandos;
+	begin
 
+		if 	ACL.Argument_Count /= 2 then
+			raise Mi_Excepcion;
+			end if;
 
+		Ada.Text_Io.Put_Line(ACL.Argument(2));
+		
+		exception
+			when Mi_Excepcion => 
+				Ada.Text_Io.Put_Line("Pon al menos un argumento");
+
+	end Linea_Comandos;
