fe110e3f2cd68bd8502fd0a79fde650b02cd50e6
Julio <JulioToboso@gmail.com>
2020-05-07 09:50:44 +0200

    Update words.adb

--- words.adb
+++ words.adb
@@ -1,6 +1,7 @@
-	-------------------------------------
-	--Gestión de palabras en un fichero--
-	-------------------------------------
+---------------------------------------
+-- Gestión de palabras en un fichero --
+-- Julio Toboso                      --
+---------------------------------------
 
 --Analiza las lineas del fichero y almacena --En lista dinámica-- las palabras  y su ocurrencia--
 
@@ -26,65 +27,57 @@ use Word_Lists;
 
 procedure Words is
 
---Renames--
-    package T_IO renames Ada.Text_IO;
-	package ASU renames Ada.Strings.Unbounded;
-	package ACL renames Ada.Command_Line;
-    package WL renames word_lists;
-    package WC renames Words_Command;
------
+	--Renames--
+	package T_IO 	renames Ada.Text_IO;
+	package ASU 	renames Ada.Strings.Unbounded;
+	package ACL 	renames Ada.Command_Line;
+	package WL 		renames word_lists;
+	package WC 		renames Words_Command;
+	-----
+	
 	procedure Free is new Ada.Unchecked_Deallocation(Cell, Word_List_Type);
 	
---Exceptions--
-    Wrong_Command:  Exception;
-    Wrong_N_Command:Exception;
---Variables--
-    File : T_IO.File_Type;
-    List : WL.Word_List_Type;
-    File_Name: ASU.Unbounded_String;
+	--Exceptions--
+	Wrong_Command:   Exception;
+	Wrong_N_Command: Exception;
+	
+	--Variables--
+	File : T_IO.File_Type;
+	List : WL.Word_List_Type;
+	File_Name: ASU.Unbounded_String;
 
-
---Exceptions--
-    Wrong_Command:  Exception;
-    Wrong_N_Command:Exception;
---Variables--
-    File : T_IO.File_Type;
-    List : WL.Word_List_Type;
-    File_Name: ASU.Unbounded_String;
-
-begin -- Words
-    case ACL.Argument_Count is
-        when 1 =>
-          File_Name:=ASU.To_Unbounded_String(ACL.Argument(1));
-          T_IO.Open(File, T_IO.In_File, ASU.To_String(File_Name));
-	      Split.Split_Text_In_Words(File, List);
-	      T_IO.Close(File);
-	      WL.Print_All(List);
-          --WL.Delete_List(List);   --WTF No se ve????
-          WC.Dispose_List(List);
-        when 2 =>
-            if ACL.Argument(1)/="-i" then
-                    raise Wrong_Command;
-            end if;
-            File_Name:=ASU.To_Unbounded_String(ACL.Argument(2));
-            T_IO.Open(File, T_IO.In_File, ASU.To_String(File_Name));
-            Split.Split_Text_In_Words(File, List);
-            T_IO.Close(File);
-            WC.Interactive_Mode(List);
-        when others =>
-                raise Wrong_N_Command;
-        end case;
-
-exception --Words
-    when Wrong_Command =>
-        T_IO.Put_Line("Paramaters out of correct use.");
-		T_IO.Put_Line("Put the -i option for menu.");
-    when  ADA.IO_EXCEPTIONS.NAME_ERROR =>
-		      T_IO.Put_Line("You didn't select an existing file.");
-        WC.Interactive_Mode(List);
-    when Wrong_N_Command =>
-        T_IO.Put_Line("Expected number of paramaters out of range.");
-        T_IO.Put_Line("words [-i] <File.txt>");
+	begin -- Words
+		case ACL.Argument_Count is
+			when 1 =>
+				File_Name:= ASU.To_Unbounded_String(ACL.Argument(1));
+				T_IO.Open(File, T_IO.In_File, ASU.To_String(File_Name));
+				Split.Split_Text_In_Words(File, List);
+				T_IO.Close(File);
+				WL.Print_All(List);
+				--WL.Delete_List(List);   --WTF No se ve????
+				WC.Dispose_List(List);
+			when 2 =>
+				if ACL.Argument(1)/= "-i" then
+					raise Wrong_Command;
+					end if;
+				File_Name := ASU.To_Unbounded_String(ACL.Argument(2));
+				T_IO.Open(File, T_IO.In_File, ASU.To_String(File_Name));
+				Split.Split_Text_In_Words(File, List);
+				T_IO.Close(File);
+				WC.Interactive_Mode(List);
+			when others =>
+				raise Wrong_N_Command;
+				end case;
+	exception --Words
+		when Wrong_Command =>
+			T_IO.Put_Line("Paramaters out of correct use.");
+			T_IO.Put_Line("Put the -i option for menu.");
+		when  ADA.IO_EXCEPTIONS.NAME_ERROR =>
+			T_IO.Put_Line("You didn't select an existing file.");
+			WC.Interactive_Mode(List);
+		when Wrong_N_Command =>
+			T_IO.Put_Line("Expected number of paramaters out of range.");
+			T_IO.Put_Line("words [-i] <File.txt>");
 
 end Words;
 
