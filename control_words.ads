90df57507e2ffeced507c9e79b508912776b3e9d
Julio <JulioToboso@gmail.com>
2020-05-07 11:14:20 +0200

    Update control_words.ads

--- control_words.ads
+++ control_words.ads
@@ -6,17 +6,22 @@ with Word_Lists;
 
 package Control_Words is
 
-	package T_IO renames Ada.Text_IO;
+	package T_IO	renames Ada.Text_IO;
 
-	package ACL renames Ada.Command_Line;
+	package ACL 	renames Ada.Command_Line;
 
 	package Int_IO is new Ada.Text_IO.Integer_IO(Integer);
 
+	procedure 
+		lounch_control_mode(
+			selector: in out integer);
 
-procedure lounch_control_mode(  selector: in out integer);
-
-procedure Dispose_List (Word_List: in out Word_Lists.Word_List_Type);
+	procedure 
+		Dispose_List(
+			Word_List: in out Word_Lists.Word_List_Type);
 		
-procedure Activate_Control_Mode (Word_List: in out Word_Lists.Word_List_Type);
+	procedure 
+		Activate_Control_Mode(
+			Word_List: in out Word_Lists.Word_List_Type);
 	
-end Control_Words;
+	end Control_Words;
