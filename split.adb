001e41b4e0b195b4b5a4cdd114dcf4bb8dc724bd
Julio <JulioToboso@gmail.com>
2020-05-07 11:06:06 +0200

    Update split.adb

--- split.adb
+++ split.adb
@@ -51,24 +51,24 @@ package body Split is
 			Position: Integer;
 			begin --Split_Line_In_Words
 				Frase:= Clean_Word(UString.To_String(Frase));
-					while 	not Finish_Line loop 
-						Position:= UString.Index(Frase," ");
-						if Position = 0 then
-							Finish_Line:= True;
-						elsif Position = 1 then
-							Frase:= UString.Tail(
-								Frase,
-								UString.Length(Frase)-Position);
-						else 
-							Word:= UString.Head(
-								Frase,
-								Position-1);
-							WL.Add_Word(List,Word);
-							Frase:= UString.Tail(
-								Frase,
-								UString.Length(Frase)-Position);
-							end if;
-						end loop;
+				while 	not Finish_Line loop 
+					Position:= UString.Index(Frase," ");
+					if Position = 0 then
+						Finish_Line:= True;
+					elsif Position = 1 then
+						Frase:= UString.Tail(
+							Frase,
+							UString.Length(Frase)-Position);
+					else 
+						Word:= UString.Head(
+							Frase,
+							Position-1);
+						WL.Add_Word(List,Word);
+						Frase:= UString.Tail(
+							Frase,
+							UString.Length(Frase)-Position);
+						end if;
+					end loop;
 				end Split_Line_In_Words;
 
 	procedure 
